b50ac3fd53a11832d70e3665b5a94683f5074439
André <dev@nchie.se>
2025-12-08 15:11:53 +0100

    Upgrade to Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://nchie.github.io",
   integrations: [preact(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
